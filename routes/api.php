c699e56a5c404a719041c3d1bc4815d10add6216
AkinAgbejoye <akinwun@gmail.com>
2026-09-17 11:34:05 +0100

    style: normalize PHP formatting

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UrlController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/urls', [UrlController::class, 'store'])
     ->middleware('throttle:10,1');
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-
 use App\Http\Controllers\Api\UrlController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/{shortCode}', [UrlController::class, 'redirect']);
